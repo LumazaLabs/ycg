c8d0657ace1818871f69649cb492ad0f14502e8a
lukebricknell <lukecbricknell@gmail.com>
2023-01-31 13:53:03 +0200

    Cookie Bar

--- public/script.js
+++ public/script.js
@@ -19,3 +19,67 @@ document.addEventListener("keydown", function (e) {
     phoneMenu.classList.add("hidden");
   }
 });
+
+// YEAR UPDATER
+
+const today = new Date();
+const year = today.getFullYear();
+document.getElementById("year").textContent = year;
+
+// POP-UP
+
+/* COOKIE POP UP */
+const popUp = document.getElementById("pop-up");
+const acceptCookie = document.getElementById("pop-up-btn");
+const declineCookie = document.getElementById("decline-btn");
+
+// Create cookie
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// Delete cookie
+function deleteCookie(cname) {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=;" + expires + ";path=/";
+}
+
+// Read cookie
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// Set cookie consent
+function acceptCookieConsent() {
+  deleteCookie("user_cookie_consent");
+  setCookie("user_cookie_consent", 1, 30);
+  document.getElementById("pop-up").style.display = "none";
+}
+
+let cookie_consent = getCookie("user_cookie_consent");
+if (cookie_consent != "") {
+  document.getElementById("pop-up").style.display = "none";
+} else {
+  document.getElementById("pop-up").style.display = "block";
+}
+
+acceptCookie.addEventListener("click", function () {
+  acceptCookieConsent();
+});
